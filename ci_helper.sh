ac9c3c747db3728c3a089cc1beaa2a7f2c20020b
Bastian Bittorf <bb@npl.de>
2021-02-05 15:33:46 +0100

    ci_helper: support for ENV var $GITDIR

--- ci_helper.sh
+++ ci_helper.sh
@@ -26,13 +26,14 @@ while [ -n "$1" ]; do {
 [ -f "$(pwd)/$OWN_CONFIG" ] && OWN_CONFIG="$(pwd)/$OWN_CONFIG"
 [ -f "$(pwd)/$MYINIT" ] && MYINIT="$(pwd)/$MYINIT"
 
-[ -d '.git' ] && {
+[ -d "${GITDIR:=.git}" ] && (
+	cd "$GITDIR" || exit
 	[ "$GIT_REPONAME"  ] || GIT_REPONAME="$( basename "$( git rev-parse --show-toplevel )" )"
 	[ "$GIT_USERNAME"  ] || GIT_USERNAME="$( basename "$( dirname "$( git config --get remote.origin.url )" )" )"
 	[ "$GIT_SHORTHASH" ] || GIT_SHORTHASH="$( git rev-parse --short HEAD )"
 	[ "$GIT_BRANCH"    ] || GIT_BRANCH="$( git rev-parse --abbrev-ref HEAD )"
 	export GIT_REPONAME GIT_USERNAME GIT_SHORTHASH GIT_BRANCH
-}
+)
 
 cd "$( dirname "$0" )" || exit
 TMP1="$( mktemp )" || exit
